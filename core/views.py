adb53bf9b95992feae12863af30f7359fc6a6e55
jonerra <jonerra23@aim.com>
2017-07-13 00:25:00 +0000

    add homepage

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+class Home(TemplateView):
+    template_name = "home.html"
